c8f8c2bc7d8e0f63f9322eebdf39da87656ea102
Matteo <matteocaucat@gmail.com>
2025-10-23 15:28:57 +0200

    Mise à jour du README

--- tp-02.sql
+++ tp-02.sql
@@ -1,26 +1,32 @@
 CREATE TABLE fournisseur (
     id INT(11) PRIMARY KEY,
-    NOM VARCHAR(25)
+    nom VARCHAR(25) NOT NULL
 );
+
 CREATE TABLE article (
     id INT(11) PRIMARY KEY,
-    REF VARCHAR(13),
-    DESIGNATION VARCHAR(255),
-    PRIX DECIMAL(7,2),
-    ID_FOU INT(11)
+    ref VARCHAR(13) UNIQUE NOT NULL,
+    designation VARCHAR(255) UNIQUE,
+    prix DECIMAL(7,2) NOT NULL,
+    id_fou INT(11)
 );
 
 CREATE TABLE bon (
-    id INT(11),
-    NUMERO INT(11),
-    DATE_CMDE DATETIME,
-    DELAI INT(11),
-    ID_FOU INT(11)
-);
-CREATE TABLE compo (
-    id INT(11),
-    ID_ART INT (11),
-    ID_BON INT (11),
-    QTE INT(11)
+    id INT(11) PRIMARY KEY,
+    numero INT(11) NOT NULL,
+    date_cmde DATETIME DEFAULT CURRENT_TIMESTAMP,
+    delai INT(11),
+    id_fou INT(11)
 );
 
+CREATE TABLE compo (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    id_art INT(11),
+    id_bon INT(11),
+    qte INT(11) NOT NULL
+);
+
+ALTER TABLE article ADD CONSTRAINT FOREIGN KEY (id_fou) REFERENCES fournisseur(id);
+ALTER TABLE bon     ADD CONSTRAINT FOREIGN KEY (id_fou) REFERENCES fournisseur(id);
+ALTER TABLE compo   ADD CONSTRAINT FOREIGN KEY (id_art) REFERENCES article (id);
+ALTER TABLE compo   ADD CONSTRAINT FOREIGN KEY (id_bon) REFERENCES bon (id);
